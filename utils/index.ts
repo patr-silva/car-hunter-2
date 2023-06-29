305097912bf521b4c161a6201201acc1c3737724
patr-silva <patriciaresende.s92@gmail.com>
2023-06-29 18:07:31 +0100

    fetching images

--- utils/index.ts
+++ utils/index.ts
@@ -50,11 +50,11 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const { make, year, model } = car;
 
   url.searchParams.append("customer", process.env.API_IMG_KEY || "");
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(" ")[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
-  url.searchParams.append('angle', `${angle}`);
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
 
   return `${url}`;
 };
